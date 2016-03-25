b6b917f52ed0a5714324b7458c95515f4ccc2042
zhangyumin618@163.com <zhangyumin618@163.com>
2016-03-25 19:25:04 +0800

    update import_pac

--- import_pac.sh
+++ import_pac.sh
@@ -12,7 +12,7 @@ echo -n "=========================Step2: bed file to jbrowse====================
 echo -n "=========================Step3: txt to bed=================================\n";
 read -p "请输入Jbrowse目标文件名，bw文件将会移入/var/www/jbrowse/data/{name}中去 以空格键分割 :" des
 chmod 777 -R /var/www/jbrowse/data/$des/
-/var/www/jbrowse/bin/flatfile-to-json.pl --bed /var/www/front/tojbrowse/PAC_$species.bed --trackLabel "APAplant stored PAC" --out /var/www/jbrowse/data/$des/
+/var/www/jbrowse/bin/flatfile-to-json.pl --bed /var/www/front/tojbrowse/PAC_$species.bed --trackLabel "plantAPA stored PAC" --out /var/www/jbrowse/data/$des/
 jq '(.tracks[-1]) |= . +{"menuTemplate" : [{"url" : "../front/converse.php?species='$species'&chr={Seq_id}&strand={strand}&coord={start}","iconClass" : "digitIconDatabase","action" : "newwindow","label" : "go PAC","title" : "go PAC"}]}' /var/www/jbrowse/data/$des/trackList.json > /var/www/jbrowse/data/$des/trackList1.json
 rm -rf /var/www/jbrowse/data/$des/trackList.json
 mv /var/www/jbrowse/data/$des/trackList1.json /var/www/jbrowse/data/$des/trackList.json
